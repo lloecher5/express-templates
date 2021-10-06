78e8571696fb55642cb7197c0bd227498d46b3d9
Luke Loecher <ljloecher@gmail.com>
2021-10-05 20:10:51 -0400

    added nodemon

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const http = require("http");
 const express = require("express");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 3001;
 
 const app = express();
 
 const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
