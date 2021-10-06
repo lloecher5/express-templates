ff1b973eb846b6e7f551d752d7192642877d2be1
Luke Loecher <ljloecher@gmail.com>
2021-10-05 21:25:31 -0400

    added express template

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+  { name: "Blueberry Pancakes", handle: "bluepan" },
+  { name: "Chicken and Waffles", handle: "wafflechick" },
+  { name: "Bacon and Eggs", handle: "bne" },
+  { name: " dry cereal", handle: "sad" },
+];
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const express = require("express");
+const es6Renderer = require("express-es6-template-engine");
+const data = require("./data");
 
 const hostname = "127.0.0.1";
 const port = 3001;
@@ -8,8 +10,39 @@ const app = express();
 
 const server = http.createServer(app);
 
+//configure the template engine
+app.engine("html", es6Renderer);
+app.set("views", "views"); //where to look for the templates (AKA views)
+app.set("view engine", "html"); //which template enginer should express use
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("home", {
+    locals: {
+      foods: data,
+      title: "Breakfast Foods",
+    },
+    partials: {
+      head: "partials/head",
+    },
+  }); //views/home.html
+});
+
+app.get("/:handle", (req, res) => {
+  const { handle } = req.params;
+  const food = data.find((profile) => profile.handle === handle);
+  if (!food) {
+    res.status(404).send("Could not find that food");
+  } else {
+    res.render("food-profile", {
+      locals: {
+        food: food,
+        title: `Breakfast Food: ${food.name}`,
+      },
+      partials: {
+        head: "partials/head",
+      },
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
